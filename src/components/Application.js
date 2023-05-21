1513660c14af37c5c58753db77a0cbd2df0b2a9e
DanOlinski <daniel.guterres@hotmail.com>
2023-05-21 00:07:39 -0600

    implement Appointment element in Application.js file

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,9 @@ import React, {useState} from 'react';
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
+//Mock data
 const days = [
   {
     id: 1,
@@ -20,6 +22,53 @@ const days = [
     spots: 0,
   },
 ];
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+//render loop through data and one appointment for each object in the array
+const renderAppointment = Object.values(appointments).map((appointment) => {
+  return (
+  <Appointment
+    key={appointment.id}
+    {...appointment}
+  />
+)})
 
 export default function Application(props) {
   //setting the default state to Monday so that Monday is selected when the page is loaded
@@ -54,7 +103,8 @@ export default function Application(props) {
       </section>
 
       <section className="schedule"> 
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {renderAppointment}
+        <Appointment key="last" time="5pm/" />
       </section>
 
     </main>
